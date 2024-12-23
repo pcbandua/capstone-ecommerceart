79a3cb58d73400e2129745d72f6589812948464a
Paulo Bandua <pcbandua@gmail.com>
2024-12-22 18:53:18 -0800

    added Carted_Product model + data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_23_022625) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_23_024809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_23_022625) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "artist_website"
+  end
+
+  create_table "carted_products", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.integer "order_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
